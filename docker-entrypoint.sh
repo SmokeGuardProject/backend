561122c9134255f76f2e8880202f904071622b01
yehor <yehor.bielik@nure.ua>
2025-12-18 02:22:55 +0200

    fix: setup deploy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,17 +2,28 @@
 set -e
 
 echo "Waiting for PostgreSQL to be ready..."
-until nc -z -v -w30 $DB_HOST 5432 2>/dev/null; do
-  echo "aiting for database connection..."
+
+until nc -z -v -w30 $DB_HOST ${DB_PORT:-5432} 2>/dev/null; do
+  echo "Waiting for database connection..."
   sleep 2
 done
 
-echo "✅ PostgreSQL is ready!"
+echo "PostgreSQL is ready!"
 
 echo "Running database migrations..."
-npm run migration:run
 
-echo "✅ Migrations completed!"
+
+if [ "$NODE_ENV" = "production" ]; then
+  echo "Production environment detected. Using dist/..."
+
+  npx typeorm migration:run -d dist/data-source.js
+else
+  echo "Development environment detected. Using src/..."
+
+  npm run migration:run
+fi
+
+echo "Migrations completed!"
 
 echo "Starting the application..."
 exec "$@"
